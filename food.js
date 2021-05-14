b983583f6ae4e3eee1b0f7f0a27443c75e638a0a
MohitWaykole <74908676+MohitWaykole@users.noreply.github.com>
2021-05-14 19:54:46 +0530

    Update food.js

--- food.js
+++ food.js
@@ -2,7 +2,7 @@ class Food{
     constructor(){
         this.foodStock;
         this.lastFed;
-        this.image = loadImage("images/Milk.png");
+        this.image = loadImage("Milk.png");
     }
 
     display(){
